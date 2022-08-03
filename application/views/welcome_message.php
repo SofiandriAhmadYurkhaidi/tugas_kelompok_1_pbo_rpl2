cd163a4026df8e659aa583debaf95b378c37d353
SofiandriAhmadYurkhaidi <sofyandriyurka01@gmail.com>
2022-08-03 10:37:00 +0700

    satu

---
+++ application/views/welcome_message.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+<style>
+	* {
+		margin:0;
+		padding:0;
+	}
+	#table {
+		width:80%;
+	}
+	ul li {
+		list-style:none;
+		display:inline-block;
+		
+	}
+	table {
+		margin:5px;
+	}
+	table, tr, td {
+  border: 3px solid black;
+  border-collapse: collapse;
+}
+
+a {
+	text-decoration:none;
+	font-weight:bold;
+}
+</style>
+
+	<center>
+		
+	<table id="table" border="1" >
+		<tr style="background-color:#EBEBD3;height:150px;width:100%;">
+	<td>
+	<center>
+	<h1><b>CI 3</b></h1>
+	</center>	
+</td>
+		</tr>
+		<tr style="background:#F5D547;">
+			<td  style="padding:10px;">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Gallery</a></li>
+				<li><a href="">About</a></li>
+			</ul>
+			</td>
+		</tr>
+		<tr style="height:400px;background:#1446A0;">
+			<td></td>
+		</tr>
+		<tr style="background:#3C3C3B;">
+		<td style="padding:10px;">
+	<center>	<b><i>Copyright &copy 2022
+</i></b></center>
+		</td>
+		</tr>
+	</table>
+	
+	</center>
+</body>
+</html>
